12ad55f724c68ba51c63169e9f3bedcd5c5401da
Ron Nahmias <ron.nahmias2@gmail.com>
2024-08-06 23:31:01 +0300

    fix env loading file to app

--- app/core/config.py
+++ app/core/config.py
@@ -1,9 +1,10 @@
-import os
-from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-load_dotenv()
 
-class Settings:
-    SQLALCHEMY_DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite:///./book_library.db")
+class Settings(BaseSettings):
+    SQLALCHEMY_DATABASE_URL: str = "sqlite:///./book_library.db"
+    
+    model_config = SettingsConfigDict(env_file=".env")
+
 
 settings = Settings()
